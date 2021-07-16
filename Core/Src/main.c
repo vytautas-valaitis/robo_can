589f1747d34012222e50f1823687022518032ee4
vytautas-valaitis <valaitisv@gmail.com>
2021-07-16 17:37:12 +0300

    can taimingai

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -64,19 +64,15 @@ static void MX_GPIO_Init(void);
 static void MX_CAN1_Init(void);
 static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
+void CAN_Config(CAN_HandleTypeDef *phcan, uint8_t FIFO_Num);
 
+uint8_t Can_TxMessage(CAN_HandleTypeDef *phcan, uint8_t ide, uint32_t id, uint8_t len, uint8_t *pdata);
+
+void Can_Dome(CAN_HandleTypeDef *hcan1);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-CAN_TxHeaderTypeDef   TxHeader;
-uint8_t               TxData[8];
-uint32_t              TxMailbox;
-
-CAN_RxHeaderTypeDef   RxHeader;
-uint8_t               RxData[8];
-
-uint8_t datacheck = 0;
 
 /* USER CODE END 0 */
 
@@ -115,95 +111,37 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  //HAL_CAN_Start(&hcan1);
 /*
-  CAN_FilterTypeDef canfilterconfig;
-
-  canfilterconfig.FilterActivation = CAN_FILTER_ENABLE;
-  canfilterconfig.FilterBank = 18;  // which filter bank to use from the assigned ones
-  canfilterconfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
-  canfilterconfig.FilterIdHigh = 0x103<<5;
-  canfilterconfig.FilterIdLow = 0;
-  canfilterconfig.FilterMaskIdHigh = 0x100<<5;
-  canfilterconfig.FilterMaskIdLow = 0x0000;
-  canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
-  canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
-  canfilterconfig.SlaveStartFilterBank = 20;  // how many filters to assign to the CAN1 (master can)
-
-  HAL_CAN_ConfigFilter(&hcan1, &canfilterconfig);
-*/
-
-
-/*
-  	  CAN_FilterTypeDef filter = {0};
-  	  filter.FilterIdHigh = 0x1fff;
-  	  filter.FilterIdLow = 0xffff;
-  	  filter.FilterMaskIdHigh = 0;
-  	  filter.FilterMaskIdLow = 0;
-  	  filter.FilterFIFOAssignment = CAN_RX_FIFO0;
-  	  filter.FilterBank = 0;
-  	  filter.FilterMode = CAN_FILTERMODE_IDMASK;
-  	  filter.FilterScale = CAN_FILTERSCALE_16BIT;
-  	  filter.FilterActivation = ENABLE;
-  	  filter.SlaveStartFilterBank = 0;
-  	  if (HAL_CAN_ConfigFilter(&hcan1, &filter) != HAL_OK)
-  	      return (1);
-
-*/
   TxHeader.IDE = CAN_ID_STD;
   TxHeader.StdId = 0x141;
   TxHeader.RTR = CAN_RTR_DATA;
-  TxHeader.DLC = 8;
+  TxHeader.DLC = 8;*/
 
-  TxData[0] = 0x9A;
-  TxData[1] = 0x00;
-  TxData[2] = 0x00;
-  TxData[3] = 0x00;
-  TxData[4] = 0x00;
-  TxData[5] = 0x00;
-  TxData[6] = 0x00;
-  TxData[7] = 0x00;
 
-  if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
-   {
-	  printf("blogai2\r\n");
- 	  //Error_Handler();
-   }
-HAL_CAN_Start(&hcan1);
+
+  CAN_Config(&hcan1, 0);
+  //CAN_Config(&hcan1, 1);
 
   while (1)
   {
-	  if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK)
-	  {
-		 printf("blogai 1\r\n");
-		 Error_Handler ();
-	  };
 
-	//  while(HAL_CAN_IsTxMessagePending(&hcan1, TxMailbox)) {
-	//  HAL_CAN_GetTxMailboxesFreeLevel(&hcan1);
-	//  };
-
-
-
-	  //printf("%s", TxMailbox);
-
-
-
-	  printf("heartbeat\r\n");
+	  printf("heartbeat (%d free)\r\n", HAL_CAN_GetTxMailboxesFreeLevel(&hcan1));
 	  HAL_Delay(500);
 
+	  uint8_t sta;
+
+      robo_status();
+
+	  if(sta != 0)
+	  {
+	     printf("err:%d\r\n", sta);
+	  }
+
+	 // Can_Dome(&hcan1);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  if (datacheck)
-	  {
-		printf("check\r\n");
-	    //printf("%s", RxData);
-	  }
-  	  //HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-  	  //HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
   	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
-
   }
   /* USER CODE END 3 */
 }
@@ -285,11 +223,11 @@ static void MX_CAN1_Init(void)
 
   /* USER CODE END CAN1_Init 1 */
   hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 18;
+  hcan1.Init.Prescaler = 6; //18; //18;
   hcan1.Init.Mode = CAN_MODE_NORMAL;
   hcan1.Init.SyncJumpWidth = CAN_SJW_2TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_1TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_4TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_4TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = DISABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
@@ -441,20 +379,189 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+uint8_t Can_TxMessage(CAN_HandleTypeDef *phcan, uint8_t ide, uint32_t id, uint8_t len, uint8_t *pdata)
+{
+    uint32_t  TxMailbox;           //Get the email number that was sent
+    CAN_TxHeaderTypeDef TxHeader;  //Send-header protocol information structure, used to fill parameters
+    HAL_StatusTypeDef   HAL_RetVal; //CAN return value
+    uint16_t i = 0;
+    /*Fill and send header protocol*/
+    if(ide == 0)
+    {
+        TxHeader.IDE = CAN_ID_STD;
+        TxHeader.StdId = id;
+    }
+    else
+    {
+        TxHeader.IDE = CAN_ID_EXT;
+        TxHeader.ExtId = id;
+    }
+
+    TxHeader.RTR = CAN_RTR_DATA,          //The frame type of the message data frame
+    TxHeader.DLC = len,                   //The length of the frame 8
+    TxHeader.TransmitGlobalTime = DISABLE; //Do not capture time
+
+    /*Ask if CAN has a free mailbox*/
+    while(HAL_CAN_GetTxMailboxesFreeLevel(phcan) == 0)
+    {
+        i++;
+        if(i > 0xfffe) {//Timeout, sending failed
+            return 1;
+        }
+    }
+    HAL_Delay(1);
+
+    /*Send frame*/
+    HAL_RetVal = HAL_CAN_AddTxMessage(phcan, &TxHeader, pdata, &TxMailbox); //Send a frame of data
+    printf("TxMailbox %d\r\n",TxMailbox);
+    if(HAL_RetVal != HAL_OK)
+        return 1;
+    return 0;
+}
+
+/*For loopback test*/
+void Can_Dome(CAN_HandleTypeDef *hcan1)
+{
+    uint8_t sta;
+    sta = Can_TxMessage(hcan1, 0, 8, 8, "789456");
+    if(sta != 0)
+    {
+        printf("err:%d\r\n", sta);
+    }
+}
+
+void robo_status()
+{
+
+    uint8_t sta;
+
+    uint8_t TxData[8];
+    TxData[0] = 0x94;
+    TxData[1] = 0x00;
+    TxData[2] = 0x00;
+    TxData[3] = 0x00;
+    TxData[4] = 0x00;
+    TxData[5] = 0x00;
+    TxData[6] = 0x00;
+    TxData[7] = 0x00;
+
+    sta = Can_TxMessage(&hcan1, CAN_ID_STD, 0x141, 8, &TxData);
+    if(sta != 0)
+    {
+        printf("err:%d\r\n", sta);
+    }
+}
+
+
+void CAN_Config(CAN_HandleTypeDef *phcan, uint8_t FIFO_Num)
+{
+	 /*
+    //Configure the filter to receive ID frames in the specified range
+    CAN_FilterTypeDef CAN_FilterType;
+    CAN_FilterType.FilterBank = 0;                        //Filter group [0,13]
+    CAN_FilterType.SlaveStartFilterBank = 14;             //Start from the filter group [0,27]
+    CAN_FilterType.FilterIdHigh = 0x0000;                 //ID high bit to be filtered [0x0000,0xFFFF]
+    CAN_FilterType.FilterIdLow = 0x0000;                  //ID to be filtered low [0x0000,0xFFFF]
+    CAN_FilterType.FilterMaskIdHigh = 0x0000;             //The high 16 bits of the filter must not match
+    CAN_FilterType.FilterMaskIdLow = 0x0000;              //The lower 16 bits of the filter must not match
+    CAN_FilterType.FilterFIFOAssignment = FIFO_Num;       //The filter is associated to (0=RX_FIFO0/1=RX_FIFO1)
+    CAN_FilterType.FilterMode = CAN_FILTERMODE_IDMASK;    //Work in identifier mask bit mode
+    CAN_FilterType.FilterScale = CAN_FILTERSCALE_32BIT;   //The filter bit width is a single 32 bits
+    CAN_FilterType.FilterActivation = ENABLE;             //Enable filter
+    if(HAL_CAN_ConfigFilter(phcan, &CAN_FilterType) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    */
+    /*Open the interrupt service corresponding to the CAN channel*/
+    if(FIFO_Num == 0)
+    {
+        if(HAL_CAN_ActivateNotification(phcan, CAN_IT_RX_FIFO0_MSG_PENDING ) != HAL_OK)
+        {
+            Error_Handler();
+        }
+    }
+    else
+    {
+        if(HAL_CAN_ActivateNotification(phcan, CAN_IT_RX_FIFO1_MSG_PENDING ) != HAL_OK)
+        {
+            Error_Handler();
+        }
+    }
+
+    /*Start CAN communication*/
+    if(HAL_CAN_Start(phcan) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+
+/*********************************************
+ Function name: HAL_CAN_RxFifo0MsgPendingCallback
+ Function: CAN channel 0 receives the callback function, and it will be triggered when a complete frame arrives.
+ Formal parameter: hcan --CAN information structure (hcan/hcan1/hcan2)
+ return value:
+ Remarks:
+**********************************************/
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
-  if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK)
-  {
-	  printf("blogai interupte");
-    Error_Handler();
-  }
+    uint8_t Rxbuff[8] = "";         //Store frame data
+    CAN_RxHeaderTypeDef RxHeader;   //Store frame header protocol
+    HAL_StatusTypeDef   HAL_RetVal; //CAN return value
 
-  //if ((RxHeader.StdId == 0x141))
- // {
-	  datacheck = 1;
- // }
+    if(hcan->Instance == CAN1)
+    {
+
+        HAL_RetVal = HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, Rxbuff); //Read a frame from channel 0 buffer
+        if(HAL_RetVal == HAL_OK)
+        {
+            /*User-defined area*/
+            if(RxHeader.IDE == 0)
+            {
+                printf("FIFO0,ID:%d -- Rxbuff:%s\r\n", RxHeader.StdId, Rxbuff);
+            }
+            else
+            {
+                printf("FIFO0,ID:%d -- Rxbuff:%s\r\n", RxHeader.ExtId, Rxbuff);
+            }
+        }
+    }
+    //if(hcan->Instance == CAN2)//When there are two CANs, share channel 0
 }
-/* USER CODE END 4 */
+/*********************************************
+ Function name: HAL_CAN_RxFifo1MsgPendingCallback
+ Function: CAN channel 1 receives the callback function, which will be triggered when a complete frame arrives.
+ Formal parameter: hcan --CAN information structure (hcan/hcan1/hcan2)
+ return value:
+ Remarks:
+**********************************************/
+void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+    uint8_t Rxbuff[8] = "";         //Store frame data
+    CAN_RxHeaderTypeDef RxHeader;   //Store frame header protocol
+    HAL_StatusTypeDef   HAL_RetVal; //CAN return value
+
+    if(hcan->Instance == CAN1)
+    {
+        HAL_RetVal = HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &RxHeader, Rxbuff); //Read a frame from the channel 1 buffer
+        if(HAL_RetVal == HAL_OK)
+        {
+            /*User-defined area*/
+            if(RxHeader.IDE == 0)
+            {
+                printf("FIFO1,ID:%d -- Rxbuff:%s\r\n", RxHeader.StdId, Rxbuff);
+            }
+            else
+            {
+                printf("FIFO1,ID:%d -- Rxbuff:%s\r\n", RxHeader.ExtId, Rxbuff);
+            }
+        }
+    }
+    //if(hcan->Instance == CAN2)//When there are two CANs, share channel 1
+}
+
 
 /**
   * @brief  This function is executed in case of error occurrence.
